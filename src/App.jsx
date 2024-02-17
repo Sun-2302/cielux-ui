47ba02aa6e38efa551b30bb63f2ae6612ae5a673
Sullivan Joro <119728329+Sullivan1301@users.noreply.github.com>
2024-02-17 20:52:58 +0300

    First version of Drive clone: add classe to upload file
    * fix: preprod have bugs so i push to preprod before create new preprod
    
    * chore: conflits

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,26 @@
 /* eslint-disable no-unused-vars */
-import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
-import Login from './Login';
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Navigate } from 'react-router-dom';
+import Home from './Home';
+import AddFile from './AddFile';
+import AddFolder from './AddFolder';
+import Logout from './Logout';
+import Login from './Login';
 import Register from './Register';
 
 function App() {
   return (
-    <div className='flex h-screen'>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Navigate to="/login" />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/register' element={<Register />} />
-          {/* Ajoutez d'autres routes ici */}
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <Router>
+      <Switch>
+      <Route path='/' element={<Navigate to="/login" />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path="/Home" exact component={Home} />
+        <Route path="/add-file" exact component={AddFile} />
+        <Route path="/add-folder" exact component={AddFolder} />
+        <Route path="/logout" exact component={Logout} />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/Home.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import Navbar from './Navbar';
+import FileList from './FileList';
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar />
+            <FileList />
+        </div>
+    );
+}
+
+export default Home;
