05d9eb4bad76debcdbfde2b2a9998d056ccc7800
Sullivan1301 <jorosullivan13@gmail.com>
2024-02-17 21:10:21 +0300

    feat: add more informations for file uploaded

--- src/FileList.jsx
+++ src/FileList.jsx
@@ -17,6 +17,12 @@ const FileList = () => {
             });
     }, []);
 
+    // Fonction pour gérer le partage d'un fichier
+    const handleShare = (fileId) => {
+        // Implémentez la logique de partage ici, par exemple, ouvrez une boîte de dialogue de partage
+        console.log('Sharing file with ID:', fileId);
+    };
+
     return (
         <div>
             <h2>Uploaded Files</h2>
@@ -25,7 +31,13 @@ const FileList = () => {
                     <li key={file.id}>
                         <span>{file.name}</span>
                         <span>{file.type}</span>
-                        {/* Ajoutez d'autres informations sur le fichier si nécessaire */}
+                        <span>{file.size}</span>
+                        <span>{file.owner}</span>
+                        <span>{file.lastAccess}</span>
+                        <span>{file.modified}</span>
+                        {/* Options d'action pour le partage et autres */}
+                        <button onClick={() => handleShare(file.id)}>Share</button>
+                        {/* Ajoutez d'autres boutons ou icônes pour d'autres actions */}
                     </li>
                 ))}
             </ul>
