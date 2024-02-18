370b72ce97ebd0d5d0712b4bbf1fd476273bbfdd
Sullivan1301 <jorosullivan13@gmail.com>
2024-02-18 22:02:23 +0300

    fix: use Link to Logout so rewrite the logic of deconnexion

--- src/Logout.jsx
+++ src/Logout.jsx
@@ -1,24 +1,22 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
-import { useHistory } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Logout = () => {
-    const history = useHistory();
-
-    // Fonction de déconnexion
     const handleLogout = () => {
         // Supprimer les informations d'authentification du stockage local ou de session
         localStorage.removeItem('token'); // Vous pouvez adapter ceci en fonction de votre méthode d'authentification
+        // Vous pouvez également ajouter d'autres actions de déconnexion ici
 
-        // Rediriger l'utilisateur vers la page de connexion
-        history.push('/login');
+        window.location.href = '/login'; // Rediriger vers la page de connexion
     };
 
     return (
         <div>
             <h2>Logout</h2>
-            <p>Êtes-vous sûr de vouloir vous déconnecter?</p>
-            <button onClick={handleLogout}>Déconnexion</button>
+            <p>Are you sure you want to logout?</p>
+            {/* Utilisation de Link pour rediriger vers une autre page */}
+            <button onClick={handleLogout}>Logout</button>
         </div>
     );
 }
